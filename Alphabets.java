215021037e6dcbbf1e7e07345a9206be2aa274d7
kanishkaa11 <153438088+kanishkaa11@users.noreply.github.com>
2025-08-28 23:36:55 +0530

    print alphabets

---
+++ Alphabets.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        for(int i=1;i<=a;i++){
+            System.out.println("a b c d e f g h i j k l m n o p q r s t u v w x y z");
+        }
+    }
+}
