1fdda081f3a2175c604885ba8dfef7be741bf77f
kanishkaa11 <153438088+kanishkaa11@users.noreply.github.com>
2025-08-28 23:58:18 +0530

    values of quotient and remainder

---
+++ Quotient.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n1=sc.nextInt();
+        int n2=sc.nextInt();
+        float a=(float)n1/n2;
+        int b=n1%n2;
+        System.out.printf("%d / %d = %.6f%n", n1,n2,a);
+        System.out.print(n1+" % "+n2+" = "+b);
+    }
+}
