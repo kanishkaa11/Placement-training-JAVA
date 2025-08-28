11b7f5648688412658feadbc1d670f24ea41c9c1
kanishkaa11 <153438088+kanishkaa11@users.noreply.github.com>
2025-08-28 23:43:46 +0530

    Count odd and even.java

---
+++ Countoddeven.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int count=0;
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            if(a[i]%2==0)
+                count++;
+        }
+        System.out.println("Odd = "+(n-count));
+        System.out.println("Even = "+count);
+    }
+}
