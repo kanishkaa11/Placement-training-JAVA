71bdc208fc9531b8b0d64705688455a1cfb7e406
kanishkaa11 <153438088+kanishkaa11@users.noreply.github.com>
2025-08-28 23:27:31 +0530

    Odd numbers

---
+++ Odd.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Odd {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            if(i%2!=0){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
