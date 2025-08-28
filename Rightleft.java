8218cdcb487b7814a6dcd47833306fb9a357e228
kanishkaa11 <153438088+kanishkaa11@users.noreply.github.com>
2025-08-28 23:17:40 +0530

    Rightleft

---
+++ Rightleft.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Rightleft {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int d,rev=0;
+        while(n>0){
+            d=n%10;
+            System.out.print(d+" ");
+            n=n/10;
+        }
+    }
+}
